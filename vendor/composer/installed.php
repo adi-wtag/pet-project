7f9e1b2f7db8899c5a9927b10f55820ba3b26258
adi-wtag <arafat.dipto@welldev.io>
2021-03-07 10:55:52 +0600

    Intit 2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'dd4f59b6dfd4b65353fdaae989f1d2dfb5f0753a',
+    'name' => 'geerlingguy/drupal-vm',
+  ),
+  'versions' => 
+  array (
+    'geerlingguy/drupal-vm' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dd4f59b6dfd4b65353fdaae989f1d2dfb5f0753a',
+    ),
+  ),
+);
